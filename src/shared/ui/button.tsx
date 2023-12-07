a7ac6541b9f1fa93352907ab56eebd3aafa395e1
Александр Комзольцев <a.komzoltsev@MacBook-Pro.local>
2023-12-07 15:01:12 +0300

    fix: makes linting edits

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -1,6 +1,6 @@
 import { splitProps } from 'solid-js'
 
-import { rippleClick, className as cn  } from '@/shared/lib'
+import { rippleClick, className as cn } from '@/shared/lib'
 
 import type { Component, JSX } from 'solid-js'
 
