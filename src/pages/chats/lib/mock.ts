171fb783603d6eb7b301bb82a174c5be8d76dc7b
Александр Комзольцев <a.komzoltsev@MacBook-Pro.local>
2023-12-10 11:47:20 +0300

    fix: returns base message & name

--- src/pages/chats/lib/mock.ts
+++ src/pages/chats/lib/mock.ts
@@ -5,13 +5,14 @@ const generateMockChatData = (): IChatItem[] => {
 
   for (let i = 0; i < 100; i++) {
     chatData.push({
+      id: crypto.randomUUID(),
       name: `User${i + 1}`,
       time: `${Math.floor(Math.random() * 24)
         .toString()
         .padStart(2, '0')}:${Math.floor(Math.random() * 60)
         .toString()
         .padStart(2, '0')}`,
-      message: `Random message lorem ipsum dolor sit, amet consectetur adipisicing elit ${i + 1}`,
+      message: `Random message ${i + 1}`,
       count: Math.floor(Math.random() * 10),
       image: 'https://placehold.co/600x400'
     })
