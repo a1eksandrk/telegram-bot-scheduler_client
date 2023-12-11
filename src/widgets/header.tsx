f1adba37653cd5b262e6ea2029218e1c0ef41fc7
Александр Комзольцев <a.komzoltsev@MacBook-Pro.local>
2023-12-11 18:06:44 +0300

    fix: corrects styles and adds a search event

--- src/widgets/header.tsx
+++ src/widgets/header.tsx
@@ -5,17 +5,20 @@ import type { Component, JSX } from 'solid-js'
 
 interface IHeaderProps {
   control?: JSX.Element
+  onSearch?: (value: string) => void
 }
 
 export const Header: Component<IHeaderProps> = props => {
+  const handleSearch = (value: string): void => props.onSearch?.(value)
+
   return (
     <header class={ cn(
-      'flex items-center gap-1 h-14 pl-4 pr-4',
+      'flex items-center gap-1 min-h-[3.5rem] h-14 pl-4 pr-4',
       'border-b border-border-color'
     ) }>
       { props.control }
 
-      <SearchInput />
+      <SearchInput onSearch={ handleSearch } />
     </header>
   )
 }
