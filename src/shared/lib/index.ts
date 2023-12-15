aea2abfaa640a2866127ed77ee3c99d6ffc59d64
Александр Комзольцев <a.komzoltsev@MacBook-Pro.local>
2023-12-15 13:16:53 +0300

    fix: adds data mocs

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -2,8 +2,12 @@ import createForm from './create-form'
 import className from './class-name'
 import setProperty from './set-property'
 import rippleClick from './ripple-click'
+import useFetchedRouteData from './use-fetched-route-data'
+
+export * from './mock'
 
 export const isBoolean = (entity: unknown): entity is boolean => typeof entity === 'boolean'
+export const isFunction = (entity: unknown): entity is ((...args: any) => any) => entity instanceof Function
 export const isEmptyString = (s: string): boolean => s.length === 0
 
-export { createForm, className, setProperty, rippleClick }
+export { createForm, className, setProperty, rippleClick, useFetchedRouteData }
